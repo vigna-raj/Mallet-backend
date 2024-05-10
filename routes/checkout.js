da0d2946cd159438c4894e97ecf9489c709b4510
vigna-raj <vignaraj5000@gmail.com>
2024-05-10 22:42:25 +0530

    fix:stripe

--- routes/checkout.js
+++ routes/checkout.js
@@ -6,52 +6,34 @@ const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
 const Product = require('../models/Product');
 
 //Route 1 for checkout using POST Auth required
-const storeItems = new Map([
-    [1, { priceInCents: 10000, name: "Learn React Today" }],
-    [2, { priceInCents: 20000, name: "Learn CSS Today" }],
-])
+
 router.post("/api/pay", authenticateToken, async (req, res) => {
-    // try {
-    // const lineitems = 
-
-    // console.log(lineitems)
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        mode: "payment",
-        line_items: await Promise.all(req.body.items.map(async item => {
-            const storeItem = await Product.findOne({ _id: item.id });
-            return {
-                price: {
-                    currency: "usd",
-                    product_data: {
-                        name: storeItem.name,
+    try {
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            mode: "payment",
+            line_items: await Promise.all(req.body.items.map(async item => {
+                const storeItem = await Product.findOne({ _id: item.id });
+                return {
+                    price_data: {
+                        currency: "inr",
+                        product_data: {
+                            name: storeItem.name,
+                        },
+                        unit_amount: (storeItem.price * 100).toFixed(0),
                     },
-                    unit_amount: (storeItem.price * 100).toFixed(0),
-                },
-                quantity: item.quantity
-            };
+                    quantity: item.quantity
+                };
 
-        }))
-    })
-    res.json({ url: session.url })
-    // } catch (e) {
-    //     res.status(500).json({ error: e })
-    // }
+            })),
+            success_url: "https://google.com", //to be filled
+            cancel_url: "https://google.com", // to be filled
+        })
+        res.json({ url: session.url })
+    } catch (e) {
+        res.status(500).json({ error: e })
+    }
 })
 
 
-// line_items: await Promise.all(req.body.items.map(async item => {
-//     const storeItem = await Product.findOne({ _id: item.id });
-//     return {
-//         price: {
-//             currency: "usd",
-//             product_data: {
-//                 name: storeItem.name,
-//             },
-//             unit_amount: (storeItem.price * 100).toFixed(0),
-//         },
-//         quantity: item.quantity
-//     };
-
-// }))
 module.exports = router
